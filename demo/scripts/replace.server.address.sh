8578fdf95ba816d872d79ab0346d22375067de47
Mike Velluet <mike.velluet@gmail.com>
2020-12-08 16:56:35 +0000

    Fix  docker cors

--- demo/scripts/replace.server.address.sh
+++ demo/scripts/replace.server.address.sh
@@ -7,8 +7,6 @@ echo "JSON files detected :"
 find $1 -type f -name "settings.json"
 find $1 -type f -name "datasources.json"
 
-find $1 -type f -name "settings.json" -exec sed -Ei 's#"/assets/siscc/|"http://'$3':5007/assets/siscc/#"http://'$2':5007/assets/siscc/#g' {} +
-
 find $1 -type f -name "settings.json" -exec sed -Ei 's#"http://sfs-qa-oecd.redpelicans.com/api"|"http://'$3':3004/api"#"http://'$2':3004/api"#g' {} +
 
 find $1 -type f -name "settings.json" -exec sed -Ei 's#"http://dv-qa-oecd.redpelicans.com"|"http://'$3':7002"#"http://'$2':7002"#g' {} +
