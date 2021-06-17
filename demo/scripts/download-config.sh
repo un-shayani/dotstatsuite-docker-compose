f444ad01250f397699ae781502bf2047f13d5ad4
Zsolt Lenart <zsolt.lenart@oecd.org>
2021-06-17 10:10:55 +0200

    Workaround for JS config issue

--- demo/scripts/download-config.sh
+++ demo/scripts/download-config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
 rm -rf config/
-git clone -b master https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-config-data.git config
+git clone -b develop https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-config-data.git config
