c5584cb8f4e1c10e4c72dc9d07b08f96b2548c12
Zsolt Lenart <zsolt.lenart@oecd.org>
2021-06-30 16:19:28 +0200

    Workaround for DLM is not loading issue https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-docker-compose/-/issues/21

--- demo/scripts/download-config.sh
+++ demo/scripts/download-config.sh
@@ -1,3 +1,4 @@
 #!/bin/sh 
 rm -rf config/
 git clone -b develop https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-config-data.git config
+git -C ./config checkout c8463380d988f0aa3f861125ca3f7a12444f61b6
