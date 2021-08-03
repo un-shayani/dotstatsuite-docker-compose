8ed9a1a7a6df0199810820b17f18d0b78020f6b5
Dimitri Roncoli <dimitri.roncoli@redpelicans.com>
2021-08-03 08:14:51 +0000

    fix default tenants on js services

--- demo/scripts/download-config.sh
+++ demo/scripts/download-config.sh
@@ -1,6 +1,3 @@
 #!/bin/sh 
 rm -rf config/
-#git clone -b master https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-config-data.git config
-# Temporary workaround:
-git clone -b develop https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-config-data.git config
-git -C ./config checkout c8463380d988f0aa3f861125ca3f7a12444f61b6
+git clone -b master https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-config-data.git config
