246149b2403017369df75b3c2cc973b860ed6edf
Zsolt Lenart <zsolt.lenart@oecd.org>
2023-05-24 15:15:10 +0200

    SSL verification turned off at cloning of config repo to aviod SSL certificate problem about local issuer certificate

--- demo/scripts/download-config.sh
+++ demo/scripts/download-config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
 rm -rf config/
-git clone -b master https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-config-data.git config
+git clone -c http.sslVerify=false -b master https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-config-data.git config
